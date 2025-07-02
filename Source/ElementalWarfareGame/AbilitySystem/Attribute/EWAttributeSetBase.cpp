9f4e5aebffdd084f596b80228c14f98fd1c0537b
Z-HongYuan <1939218189@qq.com>
2025-07-02 19:11:23 +0800

    Update: 添加MaxMoveSpeed,Health,MaxHealth

--- Source/ElementalWarfareGame/AbilitySystem/Attribute/EWAttributeSetBase.cpp
+++ Source/ElementalWarfareGame/AbilitySystem/Attribute/EWAttributeSetBase.cpp
@@ -2,3 +2,62 @@
 
 
 #include "EWAttributeSetBase.h"
+#include "GameplayEffectExtension.h"
+#include "Net/UnrealNetwork.h"
+
+UEWAttributeSetBase::UEWAttributeSetBase()
+{
+}
+
+void UEWAttributeSetBase::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	/*`REPNOTIFY_Always` 告诉 `OnRep` 函数在本地值已经等于从服务器复制的值（由于预测）时触发。默认情况下，如果本地值与从服务器复制的值相同，则不会触发 `OnRep` 函数。*/
+	DOREPLIFETIME_CONDITION_NOTIFY(UEWAttributeSetBase, Health, COND_None, REPNOTIFY_Always); //确保属性终身复制
+	DOREPLIFETIME_CONDITION_NOTIFY(UEWAttributeSetBase, MaxHealth, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UEWAttributeSetBase, MaxMoveSpeed, COND_None, REPNOTIFY_Always);
+}
+
+void UEWAttributeSetBase::OnRep_Health(const FGameplayAttributeData& OldHealth)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UEWAttributeSetBase, Health, OldHealth);
+}
+
+void UEWAttributeSetBase::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UEWAttributeSetBase, MaxHealth, OldMaxHealth);
+}
+
+void UEWAttributeSetBase::OnRep_MaxMoveSpeed(const FGameplayAttributeData& OldMoveSpeed)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UEWAttributeSetBase, MaxMoveSpeed, OldMoveSpeed);
+}
+
+void UEWAttributeSetBase::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+	Super::PreAttributeChange(Attribute, NewValue);
+
+	if (Attribute == GetHealthAttribute())
+	{
+		NewValue = FMath::Clamp(NewValue, 0.0f, GetMaxHealth());
+	}
+	if (Attribute == GetMaxMoveSpeedAttribute())
+	{
+		NewValue = FMath::Clamp(NewValue, 0.0f, 9999.0f); //限制移动速度
+	}
+}
+
+void UEWAttributeSetBase::PostGameplayEffectExecute(const struct FGameplayEffectModCallbackData& Data)
+{
+	Super::PostGameplayEffectExecute(Data);
+
+	if (Data.EvaluatedData.Attribute == GetHealthAttribute())
+	{
+		SetHealth(FMath::Clamp(GetHealth(), 0.0f, GetMaxHealth()));
+	}
+	if (Data.EvaluatedData.Attribute == GetMaxMoveSpeedAttribute())
+	{
+		SetMaxMoveSpeed(FMath::Clamp(GetMaxMoveSpeed(), 0.0f, 9999.0f));
+	}
+}
--- Source/ElementalWarfareGame/AbilitySystem/Attribute/EWAttributeSetBase.h
+++ Source/ElementalWarfareGame/AbilitySystem/Attribute/EWAttributeSetBase.h
@@ -3,14 +3,64 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
 #include "AttributeSet.h"
 #include "EWAttributeSetBase.generated.h"
 
 /**
- * 
+ * 此宏定义一组用于访问和初始化属性的辅助函数。
+ *
+ * 以下宏示例：
+ * ATTRIBUTE_ACCESSORS（ULyraHealthSet， 生命值）
+ * 将创建以下函数：
+ * static FGameplayAttribute GetHealthAttribute（）;
+ * float GetHealth（） const;
+ * void SetHealth（float NewVal）;
+ * void InitHealth（float NewVal）;
+ */
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+// ATTRIBUTE_ACCESSORS(UMyHealthSet, Health)
+
+/**
+ * 基础属性
  */
 UCLASS()
 class ELEMENTALWARFAREGAME_API UEWAttributeSetBase : public UAttributeSet
 {
 	GENERATED_BODY()
+
+public:
+	UEWAttributeSetBase();
+
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+
+	//生命值
+	UPROPERTY(BlueprintReadOnly, Category = "BaseAttributes", ReplicatedUsing = OnRep_Health)
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS(UEWAttributeSetBase, Health)
+	UFUNCTION()
+	virtual void OnRep_Health(const FGameplayAttributeData& OldHealth);
+
+	//最大生命值
+	UPROPERTY(BlueprintReadOnly, Category = "BaseAttributes", ReplicatedUsing = OnRep_MaxHealth)
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(UEWAttributeSetBase, MaxHealth)
+	UFUNCTION()
+	virtual void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth);
+
+	//移动速度
+	UPROPERTY(BlueprintReadOnly, Category = "Movement", ReplicatedUsing = OnRep_MaxMoveSpeed)
+	FGameplayAttributeData MaxMoveSpeed;
+	ATTRIBUTE_ACCESSORS(UEWAttributeSetBase, MaxMoveSpeed)
+	UFUNCTION()
+	virtual void OnRep_MaxMoveSpeed(const FGameplayAttributeData& OldMoveSpeed);
+
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+
+	virtual void PostGameplayEffectExecute(const struct FGameplayEffectModCallbackData& Data) override;
 };
