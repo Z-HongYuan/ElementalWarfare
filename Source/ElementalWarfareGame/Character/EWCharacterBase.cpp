9e5cd95cb5717c2490cae3a9a7de62b1cd791d6e
Z-HongYuan <1939218189@qq.com>
2025-07-02 19:33:11 +0800

    Add: CharacterBase,带有ASC组件

---
+++ Source/ElementalWarfareGame/Character/EWCharacterBase.cpp
@@ -0,0 +1,75 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EWCharacterBase.h"
+
+
+AEWCharacterBase::AEWCharacterBase(const FObjectInitializer& ObjectInitializer):
+	Super(ObjectInitializer.SetDefaultSubobjectClass<UEW_CMC>(ACharacter::CharacterMovementComponentName))
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AEWCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AEWCharacterBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AEWCharacterBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
+void AEWCharacterBase::InitAbilitySystemComponent()
+{
+}
+
+void AEWCharacterBase::InitAttributeSet() const
+{
+	if (GetAbilitySystemComponent() and StartupAttribute)
+	{
+		GetAbilitySystemComponent()->ApplyGameplayEffectSpecToTarget(
+			*GetAbilitySystemComponent()->MakeOutgoingSpec(StartupAttribute, -1, GetAbilitySystemComponent()->MakeEffectContext()).Data.Get(),
+			GetAbilitySystemComponent()
+		);
+	}
+}
+
+void AEWCharacterBase::InitStartAbilitiesAndEffects()
+{
+	//StartUp GE
+	for (TSubclassOf<UGameplayEffect>& Effect : StartupEffects)
+	{
+		if (!Effect) continue;
+		FGameplayEffectContextHandle ContextHandle = GetAbilitySystemComponent()->MakeEffectContext();
+		FGameplayEffectSpecHandle SpecHandle = GetAbilitySystemComponent()->MakeOutgoingSpec(Effect, -1, ContextHandle);
+		GetAbilitySystemComponent()->ApplyGameplayEffectSpecToTarget(*SpecHandle.Data.Get(), GetAbilitySystemComponent());
+	}
+
+	//服务端Give
+	if (HasAuthority())
+	{
+		for (TSubclassOf<UGameplayAbility>& Ability : StartupAbilities)
+		{
+			if (!Ability) continue;
+			FGameplayAbilitySpec AbilitySpec = FGameplayAbilitySpec(Ability, -1, -1, this);
+			GetAbilitySystemComponent()->GiveAbilityWithTag(AbilitySpec);
+		}
+	}
+
+	//服务端Give
+	if (HasAuthority())
+	{
+		for (TSubclassOf<UGameplayAbility>& Ability : StartupDoOnceAbilities)
+		{
+			if (!Ability) continue;
+			FGameplayAbilitySpec AbilitySpec(Ability, -1, -1, this);
+			GetAbilitySystemComponent()->GiveAbilityAndActivateOnce(AbilitySpec);
+		}
+	}
+}
---
+++ Source/ElementalWarfareGame/Character/EWCharacterBase.h
@@ -0,0 +1,59 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemInterface.h"
+#include "GameFramework/Character.h"
+#include "Component/EW_CMC.h"
+#include "ElementalWarfareGame/AbilitySystem/EWAbilitySystemComponentBase.h"
+#include "EWCharacterBase.generated.h"
+
+UCLASS()
+class ELEMENTALWARFAREGAME_API AEWCharacterBase : public ACharacter, public IAbilitySystemInterface
+{
+	GENERATED_BODY()
+
+public:
+	AEWCharacterBase(const FObjectInitializer& ObjectInitializer);
+
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	/*
+	 * ASC和Attribute
+	 */
+	virtual UEWAbilitySystemComponentBase* GetAbilitySystemComponent() const override { return AbilitySystemComponent; }
+	UEWAttributeSetBase* GetAttributeSet() const { return AttributeSet; }
+
+protected:
+	virtual void BeginPlay() override;
+
+	/*
+	 * ASC和Attribute
+	 */
+	UPROPERTY()
+	TObjectPtr<UEWAbilitySystemComponentBase> AbilitySystemComponent;
+	UPROPERTY()
+	TObjectPtr<UEWAttributeSetBase> AttributeSet;
+
+	/*
+	 * 初始属性集
+	 * 初始GE效果
+	 * 初始能力
+	 * 初始能力并且执行一次
+	 */
+	UPROPERTY(EditDefaultsOnly, Category = "AbilitySystem")
+	TSubclassOf<UGameplayEffect> StartupAttribute;
+	UPROPERTY(EditDefaultsOnly, Category = "AbilitySystem")
+	TArray<TSubclassOf<UGameplayEffect>> StartupEffects;
+	UPROPERTY(EditDefaultsOnly, Category = "AbilitySystem")
+	TArray<TSubclassOf<UGameplayAbility>> StartupAbilities;
+	UPROPERTY(EditDefaultsOnly, Category = "AbilitySystem")
+	TArray<TSubclassOf<UGameplayAbility>> StartupDoOnceAbilities;
+
+	
+	virtual void InitAbilitySystemComponent();//初始化AbilitySystemComponent,不做任何事,将在子类中实现
+	void InitAttributeSet() const; //必须调用,全部Give的Level=-1,使用GE初始化AttributeSet
+	void InitStartAbilitiesAndEffects(); //必须调用,全部Give的Level=-1
+};
