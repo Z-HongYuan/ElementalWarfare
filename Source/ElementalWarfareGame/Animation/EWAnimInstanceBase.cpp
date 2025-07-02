2027435191e95823168956b15dfc7999cfaa77f4
Z-HongYuan <1939218189@qq.com>
2025-07-02 18:33:29 +0800

    Add: 添加带有Tag绑定的AnimInstance

---
+++ Source/ElementalWarfareGame/Animation/EWAnimInstanceBase.cpp
@@ -0,0 +1,14 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EWAnimInstanceBase.h"
+#include "AbilitySystemGlobals.h"
+
+void UEWAnimInstanceBase::SetGameplayTagPropertyMap(AActor* ListenInActor)
+{
+	if (UAbilitySystemComponent* ASC = UAbilitySystemGlobals::GetAbilitySystemComponentFromActor(ListenInActor))
+	{
+		check(ASC);
+		GameplayTagPropertyMap.Initialize(this, ASC);
+	}
+}
---
+++ Source/ElementalWarfareGame/Animation/EWAnimInstanceBase.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffectTypes.h"
+#include "Animation/AnimInstance.h"
+#include "EWAnimInstanceBase.generated.h"
+
+/**
+ * 带有Map绑定Tag的AnimInstance
+ * 需要手动调用 SetGameplayTagPropertyMap()
+ */
+UCLASS()
+class ELEMENTALWARFAREGAME_API UEWAnimInstanceBase : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	//绑定bool和Tag的函数
+	UFUNCTION(BlueprintCallable, Category = "GameplayTags")
+	void SetGameplayTagPropertyMap(AActor* ListenInActor);
+
+protected:
+	UPROPERTY(EditDefaultsOnly, Category = "GameplayTags")
+	FGameplayTagBlueprintPropertyMap GameplayTagPropertyMap;
+};
