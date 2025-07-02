30478528a3efad133676dfea0b605c64cf6e403b
Z-HongYuan <1939218189@qq.com>
2025-07-02 18:30:25 +0800

    Add: FadingActor渐隐Actor

---
+++ Source/ElementalWarfareGame/Actors/EWFadingActor.cpp
@@ -0,0 +1,72 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EWFadingActor.h"
+
+
+AEWFadingActor::AEWFadingActor()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	PoseableMesh = CreateDefaultSubobject<UPoseableMeshComponent>(TEXT("PoseableMesh"));
+
+	FadeMaterialStartValue = -1.0f;
+	FadeMaterialEndValue = 1.0f;
+	FadeStartDelayTime = FadeEndDelayTime = 1.0f;
+	bIsBeginFade = false;
+	FadeMaterialName = "FadingAlphaControl";
+}
+
+void AEWFadingActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (SourceMesh)
+	{
+		PoseableMesh->SetSkinnedAssetAndUpdate(SourceMesh->GetSkeletalMeshAsset());
+		PoseableMesh->CopyPoseFromSkeletalComponent(SourceMesh);
+
+		//淡化前的延迟
+		GetWorldTimerManager().SetTimer(FadeStartDelayTimerHandle, this, &ThisClass::FadeStartDelay, 1, false, FadeStartDelayTime);
+	}
+	else
+	{
+		//log
+		UE_LOG(LogTemp, Warning, TEXT("AGenshinFadingActor::BeginPlay() Owner is not GenshinCharacter"));
+		this->Destroy();
+	}
+}
+
+void AEWFadingActor::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	if (FadeStartDelayTimerHandle.IsValid())
+	{
+		GetWorldTimerManager().ClearTimer(FadeStartDelayTimerHandle);
+		FadeStartDelayTimerHandle.Invalidate();
+	}
+	if (FadeEndDelayTimerHandle.IsValid())
+	{
+		GetWorldTimerManager().ClearTimer(FadeEndDelayTimerHandle);
+		FadeEndDelayTimerHandle.Invalidate();
+	}
+
+	Super::EndPlay(EndPlayReason);
+}
+
+void AEWFadingActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (bIsBeginFade)
+	{
+		FadeMaterialStartValue = FMath::FInterpTo(FadeMaterialStartValue, FadeMaterialEndValue, DeltaTime, FadeSpeed);
+		PoseableMesh->SetScalarParameterValueOnMaterials(FadeMaterialName, FadeMaterialStartValue);
+	}
+
+	//淡化结束的定时器回调,无论如何,只会执行一次
+	if (FMath::IsNearlyEqual(FadeMaterialStartValue, FadeMaterialEndValue, 0.001f) and bIsBeginFade)
+	{
+		bIsBeginFade = false;
+		GetWorldTimerManager().SetTimer(FadeEndDelayTimerHandle, this, &ThisClass::FadeEndDelay, 1, false, FadeEndDelayTime);
+	}
+}
---
+++ Source/ElementalWarfareGame/Actors/EWFadingActor.h
@@ -0,0 +1,76 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/PoseableMeshComponent.h"
+#include "GameFramework/Actor.h"
+#include "EWFadingActor.generated.h"
+
+/*
+ * 通过Tick 进行透明度渐变
+ * 帧率越高,透明度变化越快
+ * 帧率越低,透明度变化越慢
+ */
+UCLASS()
+class ELEMENTALWARFAREGAME_API AEWFadingActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AEWFadingActor();
+
+	//需要复制的网格体
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Fade", meta=(ExposeOnSpawn="true"))
+	TObjectPtr<USkeletalMeshComponent> SourceMesh;
+
+	//材质淡出的开始值,默认为-1
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Fade", meta=(ExposeOnSpawn="true"))
+	float FadeMaterialStartValue;
+
+	//材质淡出的结束值,默认为1
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Fade", meta=(ExposeOnSpawn="true"))
+	float FadeMaterialEndValue;
+
+	//需要修改的材质参数值名称,默认为为"FadingAlphaControl"
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Fade", meta=(ExposeOnSpawn="true"))
+	FName FadeMaterialName;
+
+	//淡出开始前等待的延迟,默认为1
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Fade", meta=(ExposeOnSpawn="true"))
+	float FadeStartDelayTime;
+
+	//淡出完成后结束延迟,最后销毁,默认为1
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Fade", meta=(ExposeOnSpawn="true"))
+	float FadeEndDelayTime;
+
+	//淡化速度
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Fade", meta=(ExposeOnSpawn="true"))
+	float FadeSpeed;
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "PoseableMesh")
+	TObjectPtr<UPoseableMeshComponent> PoseableMesh;
+
+	//淡出控制bool
+	bool bIsBeginFade;
+
+	//定时器句柄
+	FTimerHandle FadeStartDelayTimerHandle;
+	FTimerHandle FadeEndDelayTimerHandle;
+
+	//延迟开始定时器回调,控制淡出bool
+	UFUNCTION()
+	void FadeStartDelay() { bIsBeginFade = true; }
+
+	//延迟结束定时器回调,控制销毁自身
+	UFUNCTION()
+	void FadeEndDelay() { this->Destroy(); }
+
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+};
