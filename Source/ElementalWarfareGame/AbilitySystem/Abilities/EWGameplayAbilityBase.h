ec42c6f2194324f69fb3cbfd22c7acc8092a513e
Z-HongYuan <1939218189@qq.com>
2025-07-02 18:23:10 +0800

    Add: AttributeBase,AbilityBase

---
+++ Source/ElementalWarfareGame/AbilitySystem/Abilities/EWGameplayAbilityBase.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "EWGameplayAbilityBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ELEMENTALWARFAREGAME_API UEWGameplayAbilityBase : public UGameplayAbility
+{
+	GENERATED_BODY()
+};
---
+++ Source/ElementalWarfareGame/AbilitySystem/Attribute/EWAttributeSetBase.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "EWAttributeSetBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ELEMENTALWARFAREGAME_API UEWAttributeSetBase : public UAttributeSet
+{
+	GENERATED_BODY()
+};
